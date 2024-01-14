db3f9727ba7b45c48de0d721de23229f4a9695ec
spjs <20024675@qq.com>
2024-01-14 10:37:56 +0800

    有问题，发一个消息，前端会收到两次

--- ConsoleApplication1/ConsoleApplication1/MsgQueue.cpp
+++ ConsoleApplication1/ConsoleApplication1/MsgQueue.cpp
@@ -45,11 +45,63 @@ string GbkToUtf8(const char* src_str)
 	if (str) delete[] str;
 	return strTemp;
 }
+std::string Utf8ToGbk(const std::string& str)
+{
+#if defined(_WIN32) || defined(_MSC_VER) || defined(WIN64) 
+	// calculate length
+	int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, NULL, 0);
+	wchar_t* wsGbk = new wchar_t[len + 1ull];
+	// set to '\0'
+	memset(wsGbk, 0, len + 1ull);
+	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, wsGbk, len);
+	len = WideCharToMultiByte(CP_ACP, 0, wsGbk, -1, NULL, 0, NULL, NULL);
+	char* csGbk = new char[len + 1ull];
+	memset(csGbk, 0, len + 1ull);
+	WideCharToMultiByte(CP_ACP, 0, wsGbk, -1, csGbk, len, NULL, NULL);
+	std::string res(csGbk);
+
+	if (wsGbk)
+	{
+		delete[] wsGbk;
+	}
+
+	if (csGbk)
+	{
+		delete[] csGbk;
+	}
+
+	return res;
+#elif defined(__linux__) || defined(__GNUC__)
+	size_t len = str.size() * 2 + 1;
+	char* temp = new char[len];
+	if (EncodingConvert("utf-8", "gb2312", const_cast<char*>(str.c_str()),
+		str.size(), temp, len) >= 0)
+	{
+		std::string res;
+		res.append(temp);
+		delete[] temp;
+		return res;
+	}
+	else
+	{
+		delete[] temp;
+		return str;
+	}
+
+#else
+	std::cerr << "Unhandled operating system." << std::endl;
+	return str;
+#endif
+}
+
 void MsgQueue::OnRecv(const MsgLogin& msg)
 {
+	auto utf8Name = Utf8ToGbk(msg.name);
+	std::cout << "准备广播" << utf8Name;
 	for (auto p : g_set)
 	{
-		MsgLoginRet ret = { 223,GbkToUtf8("大侠") };
+		const auto strBroadcast = "[" + utf8Name + "]进来了";
+		MsgLoginRet ret = { 223,GbkToUtf8(strBroadcast.c_str()) };
 		p->Send(ret);
 	}
 
