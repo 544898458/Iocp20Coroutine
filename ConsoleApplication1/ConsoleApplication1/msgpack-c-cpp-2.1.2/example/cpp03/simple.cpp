b06230ed7b35de308c75169a092c10ddb0ecbd75
spjs <20024675@qq.com>
2024-01-19 23:58:51 +0800

    msgpack

---
+++ ConsoleApplication1/ConsoleApplication1/msgpack-c-cpp-2.1.2/example/cpp03/simple.cpp
@@ -0,0 +1,56 @@
+// MessagePack for C++ example
+//
+// Copyright (C) 2008-2015 FURUHASHI Sadayuki and KONDO Takatoshi
+//
+//    Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//    http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <msgpack.hpp>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+struct S
+{
+    int d;
+    std::vector<int> v;
+    std::string str;
+    MSGPACK_DEFINE(d, v,str);
+};
+
+int main(void)
+{
+    
+
+    //msgpack::type::tuple<int, bool, std::string> src(1, true, "example");
+    S src = { 3,{5,3},"87¥Ûœ¿"};
+
+    // serialize the object into the buffer.
+    // any classes that implements write(const char*,size_t) can be a buffer.
+    std::stringstream buffer;
+    msgpack::pack(buffer, src);
+
+    // send the buffer ...
+    buffer.seekg(0);
+
+    // deserialize the buffer into msgpack::object instance.
+    std::string str(buffer.str());
+
+    msgpack::object_handle oh = msgpack::unpack(str.data(), str.size());
+
+    // deserialized object is valid during the msgpack::object_handle instance alive.
+    msgpack::object deserialized = oh.get();
+
+    // msgpack::object supports ostream.
+    std::cout << deserialized << std::endl;
+
+    // convert msgpack::object instance into the original type.
+    // if the type is mismatched, it throws msgpack::type_error exception.
+    //msgpack::type::tuple<int, bool, std::string> dst;
+    S dst;
+    deserialized.convert(dst);
+
+    return 0;
+}
