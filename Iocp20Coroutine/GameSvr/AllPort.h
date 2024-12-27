f65f4c1ef1659f577dd7af7f15400a968e76d670
spjs <20024675@qq.com>
2024-12-27 23:09:10 +0800

    不用443

--- Iocp20Coroutine/GameSvr/AllPort.h
+++ Iocp20Coroutine/GameSvr/AllPort.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 const uint32_t PORT_GAMESVR = 12345;
-const uint32_t PORT_GATESVR = 443;
-//const uint32_t PORT_GATESVR = 12348;
+//const uint32_t PORT_GATESVR = 443;
+const uint32_t PORT_GATESVR = 12348;
 const uint32_t PORT_WORLDSVR_ACCEPT_GAME = 12346;
 const uint32_t PORT_WORLDSVR_ACCEPT_GATE = 12349;
