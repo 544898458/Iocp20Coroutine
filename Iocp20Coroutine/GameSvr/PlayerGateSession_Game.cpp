d1e3322a153d13bce6f731c7baaead11567996f2
spjs <20024675@qq.com>
2025-03-02 15:22:27 +0800

    飞机选中音效，强行走语音

--- Iocp20Coroutine/GameSvr/PlayerGateSession_Game.cpp
+++ Iocp20Coroutine/GameSvr/PlayerGateSession_Game.cpp
@@ -288,11 +288,11 @@ CoTaskBool PlayerGateSession_Game::Co进多人联机地图(WpEntity wp视口)
 		auto& ref视口 = *wp视口.lock();
 		auto pos出生 = Position(std::rand() % 100 - 50.f, std::rand() % 100 - 50.f);
 		refSpace.造活动单位(ref视口, NickName(), 单位类型::工程车, pos出生, true);
-		refSpace.造活动单位(ref视口, NickName(), 单位类型::兵,		{ pos出生.x, pos出生.z + 6 });
-		refSpace.造活动单位(ref视口, NickName(), 单位类型::三色坦克,{ pos出生.x + 6, pos出生.z });
-		refSpace.造活动单位(ref视口, NickName(), 单位类型::工蜂,	{ pos出生.x + 6, pos出生.z + 6 });
-		refSpace.造活动单位(ref视口, NickName(), 单位类型::近战兵,	{ pos出生.x - 6, pos出生.z });
-		refSpace.造活动单位(ref视口, NickName(), 单位类型::飞机,	{ pos出生.x - 6, pos出生.z - 6 });
+		refSpace.造活动单位(ref视口, NickName(), 单位类型::兵, { pos出生.x, pos出生.z + 6 });
+		refSpace.造活动单位(ref视口, NickName(), 单位类型::三色坦克, { pos出生.x + 6, pos出生.z });
+		refSpace.造活动单位(ref视口, NickName(), 单位类型::工蜂, { pos出生.x + 6, pos出生.z + 6 });
+		refSpace.造活动单位(ref视口, NickName(), 单位类型::近战兵, { pos出生.x - 6, pos出生.z });
+		refSpace.造活动单位(ref视口, NickName(), 单位类型::飞机, { pos出生.x - 6, pos出生.z - 6 });
 
 		//auto [stop, msgResponce] = co_await AiCo::ChangeMoney(*this, 0, true, m_funCancel进地图);
 		//if (stop)
@@ -515,6 +515,7 @@ void PlayerGateSession_Game::OnRecv(const MsgMove& msg)
 			case 近战兵:播放声音("tfbYes03", ""); break;//Checked up and good to go. 检查完毕，准备动身
 			case 工程车:播放声音(msg.b遇到敌人自动攻击 ? "语音/是女声可爱版" : "语音/明白女声可爱版", ""); break;
 			case 三色坦克:播放声音("语音/ttayes01", ""); break;
+			case 飞机:播放声音(msg.b遇到敌人自动攻击 ? "音效/飞机声" : "语音/飞机明白", ""); break;
 			default:
 				continue;
 				break;
--- Iocp20Coroutine/GateSvr/GateSession.cpp
+++ Iocp20Coroutine/GateSvr/GateSession.cpp
@@ -75,7 +75,7 @@ CoTask<int> GateSession::CoLogin(MsgLogin msg, FunCancel& funCancel)
 
 	_ASSERT(!m_bLoginOk);
 	{
-		if (msg.u32版本号 < 2)
+		if (msg.u32版本号 < 3)
 		{
 			LOG(WARNING) << "版本过低:" << msg.u32版本号;
 			SendToGateClient<MsgLoginResponce>({ .result = MsgLoginResponce::客户端版本太低 }, (uint64_t)this);
