fc977239fef77929001c554173491a268bfe4307
spjs <20024675@qq.com>
2025-04-24 09:21:58 +0800

    进混战

--- Python中间人转发/乱发测试.py
+++ Python中间人转发/乱发测试.py
@@ -54,9 +54,13 @@ class MsgId(Enum):
     播放音乐 = 44
     原地坚守 = 45
     解锁单位 = 46
+    所有单位属性等级 = 47
+    升级单位属性 = 48
+    苔蔓半径 = 49
+    太岁分裂 = 50
 
 
-def onRecvWorldSvr(arr) -> bool:
+async def onRecvWorldSvr(arr,websocket) -> bool:
     idxArr = 0
     
     msg, sn, rpc = arr[idxArr]
@@ -83,12 +87,47 @@ def onRecvWorldSvr(arr) -> bool:
             print('str在线人数', str在线人数)
 
             # this.显示在线人数()
+            global login_successful  # Declare the variable as global to modify it
+            login_successful = True  # Set the flag to True
+           
+            #发消息，进多人联机混战
+            arr = [[MsgId.进Space.value, 0, 0, 0],1,]
+            print('发arr', arr)
+            await websocket.send(packb(arr))
             return True
         case _:
             print('未处理',idMsg)
             return False
     
-def 收到消息(reply) -> bool:
+async def onRecvGameSvr(arr, websocket) -> bool:
+    idxArr = 0
+    
+    msg, sn, rpc = arr[idxArr]
+    idxArr += 1
+    
+    idMsg = MsgId(msg)
+    match idMsg:
+        case MsgId.NotifyPos:
+            id = arr[idxArr];  idxArr += 1
+            posX = arr[idxArr];  idxArr += 1
+            posZ = arr[idxArr];  idxArr += 1
+            eulerAnglesY = arr[idxArr];  idxArr += 1
+            hp = arr[idxArr];  idxArr += 1
+            能量 = arr[idxArr];  idxArr += 1
+            print('NotifyPos', id)
+            
+            return True
+        case MsgId.进Space:
+            print('进Space', idMsg)
+            return True
+        case MsgId.播放音乐:
+            print('播放音乐', idMsg)
+            return True
+        case _:
+            print('onRecvGameSvr 未处理',idMsg)
+            return False
+  
+async def 收到消息(reply, websocket) -> bool:
     index = 0
     msg, sn, rpc = reply[index]
     index += 1
@@ -99,13 +138,16 @@ def 收到消息(reply) -> bool:
             print('登录成功')
             return True
         case MsgId.GateSvr转发GameSvr消息给游戏前端:
-            print('收到消息', reply[1])
+            arrGameMsg = reply[index]
+            index += 1
+            print('GateSvr转发GameSvr消息给游戏前端 收到消息', arrGameMsg)
+            await onRecvGameSvr(unpackb(arrGameMsg, strict_map_key=False), websocket)
             return True
         case MsgId.GateSvr转发WorldSvr消息给游戏前端:
             arrWorldMsg = reply[index] 
             index += 1
-            print('收到消息', arrWorldMsg)
-            onRecvWorldSvr(unpackb(arrWorldMsg))
+            print('GateSvr转发WorldSvr消息给游戏前端 收到消息', arrWorldMsg)
+            await onRecvWorldSvr(unpackb(arrWorldMsg), websocket)
             return True
         case _:
             print('未处理',idMsg)
@@ -115,7 +157,7 @@ async def main() -> None:
     # Create SSL context if necessary
     ssl_context = ssl.create_default_context()
 
-    async with websockets.connect('wss://rtsgame.online:12348', ssl=ssl_context) as websocket:
+    async with websockets.connect('wss://test.rtsgame.online:12348', ssl=ssl_context) as websocket:
         print('已连上')
         
         sendMsgSn = 0
@@ -128,19 +170,18 @@ async def main() -> None:
             [MsgId.Login.value, sendMsgSn, 0, 0],
             str登录名,
             'Hello, world!pwd',
-            10  # 版本号
+            15  # 版本号
         ]
         
         request = packb(obj)  # Pack the correct object
         await websocket.send(request)
         print('已发出')
         
-        reply = await websocket.recv()
-        obj = unpackb(reply)
-        print('reply', obj)
-        收到消息(obj)
-        
-        # Fix the undefined variable
-        await websocket.send(reply)
+        while True:
+            reply = await websocket.recv()
+            obj = unpackb(reply)
+            print('reply', obj)
+            await 收到消息(obj, websocket)
+            
 
 asyncio.run(main())
