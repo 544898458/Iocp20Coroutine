6869ec6ead977dd547675af3348c723b793678b6
spjs <544898458@qq.com>
2026-03-28 03:07:14 +0800

    Iocp20Coroutine/GameSvr/GameSvrApp.cpp 可跨平台

--- Iocp20Coroutine/GameSvr/GameSvrApp.cpp
+++ Iocp20Coroutine/GameSvr/GameSvrApp.cpp
@@ -1,9 +1,21 @@
 ﻿#include "pch.h"
+#ifdef _WIN32
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <vld.h>
-#include <string.h>
 #include "../IocpNetwork/Server.h"
+#include "../IocpNetwork/Client.h"
+#include "ClientSession_GameToWorld.h"
+#else
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include "../IocpNetwork/ServerEpoll.h"
+#include "../IocpNetwork/ClientEpoll.h"
+#include "ClientSession_GameToWorldEpoll.h"
+#endif
+#include <string.h>
 #include "../IocpNetwork/ThreadPool.h"
 #include "../CoRoutine/CoTimer.h"
 #include "Space.h"
@@ -11,7 +23,6 @@
 #include "GameSvr.h"
 #include "GameSvrSession.h"
 #include <glog/logging.h>
-#include "ClientSession_GameToWorld.h"
 #include "../IocpNetwork/MsgPack.h"
 #include "AiCo.h"
 #include "PlayerGateSession_Game.h"
@@ -21,51 +32,39 @@
 #include "单位.h"
 #include "枚举/战局类型.h"
 #include "../读Yaml配置/翻译.h"
+#include "../读Yaml配置/读GameSvr配置.h"
 
-#pragma comment(lib, "Ws2_32.lib")
-#pragma comment(lib, "Mswsock.lib")
-
-#pragma comment(lib, "glog.lib")
 
 BOOL g_running = TRUE;
+
+#ifdef _WIN32
 BOOL WINAPI fun(DWORD dwCtrlType)
 {
 	switch (dwCtrlType)
 	{
-	case CTRL_CLOSE_EVENT://控制台关闭（点右上角X关闭）
+	case CTRL_CLOSE_EVENT:
 		LOG(WARNING) << "不能点右上角X关闭，可能有数据没保存";
-		//delete g_Accept;
-		//g_Accept = nullptr;
-
-		//释放所有socket
-		//CloseHandle(hPort);
-		//Clear();
-
 		g_running = FALSE;
-
-		//释放线程句柄
-		//for (int i = 0; i < process_count; i++)
-		//{
-		//	//TerminateThread(pThread[i],);
-		//	CloseHandle(pThread[i]);
-		//}
-		//free(pThread);
 		Sleep(300000*1000);
-		_CrtDumpMemoryLeaks();	 //显示内存泄漏报告
-		//Sleep(1000);
-		//false;
+		_CrtDumpMemoryLeaks();
 		break;
 	case CTRL_C_EVENT:
 		g_running = FALSE;
-		//return false;
 		break;
 	}
 	return TRUE;
 }
+#else
+void SignalHandler(int sig)
+{
+	LOG(INFO) << "收到信号:" << sig << ",开始关闭服务器";
+	g_running = false;
+}
+#endif
 
-//std::unique_ptr<Iocp::Server<WorldClient> > g_worldSvr;// (Iocp::ThreadPool::GetIocp());
 std::unique_ptr<Iocp::SessionSocketCompletionKey<ClientSession_GameToWorld> > g_ConnectToWorldSvr;
 std::unique_ptr<Iocp::Server<GameSvr> > g_upAccept;
+
 template<class T>
 void SendToWorldSvr(const T& refMsg)
 {
@@ -91,13 +90,10 @@ template void SendToWorldSvr(const MsgChangeMoney& msg);
 template void SendToWorldSvr(const Msg战局结束& msg);
 template void SendToWorldSvr(const Msg击杀& msg);
 
-LONG WINAPI UnhandledExceptionFilter_SpawDmp(struct _EXCEPTION_POINTERS* ExceptionInfo);
-
 std::weak_ptr<PlayerGateSession_Game> GetPlayerGateSession(const std::string &refStrNickName)
 {
 	std::weak_ptr<PlayerGateSession_Game> spRet;
 	g_upAccept->m_Server.m_Sessions.ForEach([&spRet, &refStrNickName](GameSvrSession& refSession) {
-		//CHECK_NOTNULL_VOID(pSession);
 		auto &refMap = refSession.m_mapPlayerGateSession;
 		auto iterFind = std::find_if(refMap.begin(), refMap.end(),
 		[&refStrNickName](const auto& pair)->bool
@@ -112,60 +108,67 @@ std::weak_ptr<PlayerGateSession_Game> GetPlayerGateSession(const std::string &re
 
 	return spRet;
 }
-int main(void)
+
+int main(int argc, char* argv[])
 {
+	const int gameSvrId = (argc >= 2) ? atoi(argv[1]) : 0;
+
+#ifdef _WIN32
 	MiniDump::Install("GameSvr");
-	//memset((void*)12341324, 23423, 234234);
-	//屏蔽控制台最小按钮和关闭按钮
 	HWND hwnd = GetConsoleWindow();
 	HMENU hmenu = GetSystemMenu(hwnd, false);
-	//RemoveMenu(hmenu, SC_CLOSE, MF_BYCOMMAND);
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+	SetConsoleCtrlHandler(fun, TRUE);
+#else
+	signal(SIGTERM, SignalHandler);
+	signal(SIGINT, SignalHandler);
+	signal(SIGPIPE, SIG_IGN);
+#endif
 
-	auto* pMemoryLeak = malloc(123);//测试内存泄漏
+	auto* pMemoryLeak = malloc(123);
 	const char sz[] = "Memory Leak Test.Nei Cun Xie Lou Jian Ce.This is not a real Defect.Zhe Bu Shi Yi Ge Zhen De Que Xian.";
 	memcpy(pMemoryLeak, sz, sizeof(sz));
 	pMemoryLeak = nullptr;
 
-	FLAGS_alsologtostderr = true;//是否将日志输出到文件和stderr
+	FLAGS_alsologtostderr = true;
 	FLAGS_colorlogtostdout = true;
-	FLAGS_colorlogtostderr = true;//20240216
-	google::InitGoogleLogging("GameSvr");//使用glog之前必须先初始化库，仅需执行一次，括号内为程序名
+	FLAGS_colorlogtostderr = true;
+	FLAGS_log_dir = "./Log/GameSvr";
+	google::InitGoogleLogging("GameSvr");
 
 	LOG(INFO) << "Test GLOG_INFO";
 	LOG(WARNING) << "Test GLOG_WARNING";
 	LOG(ERROR) << "Test GLOG_ERROR";
-	LOG(INFO) << "GameSvr已启动";
-	CHECK_RET_DEFAULT(单位::读配置文件());
+	LOG(INFO) << "GameSvr已启动 GameSvrId=" << gameSvrId;
+	单位::读配置文件();
 	CHECK_RET_DEFAULT(读Yaml配置::读翻译配置文件("配置/翻译.yaml"));
 
-	SetConsoleCtrlHandler(fun, TRUE);
+	读Yaml配置::GameSvr配置 gameSvrConfig;
+	CHECK_RET_DEFAULT(读Yaml配置::FindGameSvr配置(gameSvrId, gameSvrConfig));
+
 	Iocp::ThreadPool threadPoolNetwork;
 	threadPoolNetwork.Init();
 
 	g_upAccept.reset(new Iocp::Server<GameSvr>(threadPoolNetwork.GetIocp()));
-	//g_worldSvr.reset( new Iocp::Server<WorldServer>(Iocp::ThreadPool::GetIocp()) );
 
 	Iocp::WsaStartup();
-	g_upAccept->Init<GameSvrSession>(PORT_GAMESVR);
-	//Iocp::ThreadPool::Add(accept.GetIocp());
+	g_upAccept->Init<GameSvrSession>(gameSvrConfig.u16端口);
 
-	//g_worldSvr->Init<WorldSession>(12346);
-	//g_worldSvr.reset(new Iocp::Server<WorldClient>(Iocp::ThreadPool::GetIocp()));
 	g_ConnectToWorldSvr.reset(Iocp::Client::Connect<ClientSession_GameToWorld>("127.0.0.1", PORT_WORLDSVR_ACCEPT_GAME, threadPoolNetwork.GetIocp()));
 	extern std::function<void(MsgSay const&)> m_funBroadcast;
 	m_funBroadcast = [](const MsgSay& msg) {g_upAccept->m_Server.m_Sessions.Broadcast(msg); };
 
-	auto wpSpace无限刷怪 = Space::AddSpace(战局类型::多玩家混战);
-	auto wpSpace圆坑 = Space::AddSpace(战局类型::多玩家混战_圆坑);
-	CHECK_WP_RET_DEFAULT(wpSpace无限刷怪);
-	CHECK_WP_RET_DEFAULT(wpSpace圆坑);
-
 	FunCancel funCancelSpawnMonster;
-	AiCo::多人联机地图(*wpSpace无限刷怪.lock(), 100, funCancelSpawnMonster).RunNew();
-	AiCo::多人联机地图(*wpSpace圆坑.lock(), 120, funCancelSpawnMonster).RunNew();
+	for (auto& spaceConfig : gameSvrConfig.vecSpaces)
+	{
+		if( 战局类型::单人ID_非法_MIN == spaceConfig.战局)
+			continue;
 
-	//主逻辑工作线程
+		auto wpSpace = Space::AddSpace(spaceConfig.战局);
+		CHECK_WP_RET_DEFAULT(wpSpace);
+		AiCo::多人联机地图(*wpSpace.lock(), (uint16_t)spaceConfig.f半径, spaceConfig.u最大怪物数, funCancelSpawnMonster).RunNew();
+	}
+
 	using namespace std;
 	std::chrono::system_clock::time_point timeLast = std::chrono::system_clock::now();
 
@@ -216,7 +219,6 @@ int main(void)
 
 	g_upAccept->Stop();
 	LOG(INFO) << "正常退出,GetCurrentThreadId=" << GetCurrentThreadId();
-	Sleep(3000);
-	//system(0);
+	std::this_thread::sleep_for(std::chrono::seconds(3));
 	return 0;
 }
--- Iocp20Coroutine/GameSvr/pch.cpp
+++ Iocp20Coroutine/GameSvr/pch.cpp
@@ -1,1 +1,5 @@
 #include "pch.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "glog.lib")
